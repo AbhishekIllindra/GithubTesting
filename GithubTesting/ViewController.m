1b6a7ef37bf229b201e140e3f650162922cfa741
AbhishekIllindra <abhiillindra@gmail.com>
2014-01-03 18:09:10 +0530

    Nslog in Viewdidload

--- GithubTesting/ViewController.m
+++ GithubTesting/ViewController.m
@@ -17,7 +17,8 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    NSLog(@"Message in view did load");
+	// Do any additional setup after loading the view, typically from a nib.
 }
 
 - (void)didReceiveMemoryWarning
